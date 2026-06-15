f15a24925b3f3fc014857ae34ee6be492cc459cc
Anil Bhukya <anilbhukya1412@gmail.com>
2026-06-15 11:51:44 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
-<<<<<<< HEAD
+
 import Quiz from './Components/Quiz'
-=======
-import Quiz from './Components/Quiz/Quiz'
->>>>>>> 8866d707b76dd76eeea414409cb764c046666133
 
 const App = () => {
   return (
